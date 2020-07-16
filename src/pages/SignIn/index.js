3cf5cc3184fc02c2ad0e03e2e6b40a372400d063
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-07-15 21:45:05 -0300

    Cria o linear gradient no background das telas de signin e singup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+import Background from '~/components/Background';
+
 // import { Container } from './styles';
 
 const SignIn = () => {
-  return <Text>SignIn</Text>;
+  return (
+    <Background>
+      <Text>SignIn</Text>
+    </Background>
+  );
 };
 
 export default SignIn;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Text } from 'react-native';
+
+import Background from '~/components/Background';
 
 // import { Container } from './styles';
 
 const SignUp = () => {
-  return <View />;
+  return (
+    <Background>
+      <Text>SignUp</Text>
+    </Background>
+  );
 };
 
 export default SignUp;
