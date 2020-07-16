49edbf0b11f90f5a4c9c07a8275777072680dd71
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-07-16 14:02:32 -0300

    Cria a página de SignUp

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,16 +1,62 @@
+/* eslint-disable react/forbid-prop-types */
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
-const SignUp = () => {
+const SignUp = ({ navigation }) => {
   return (
     <Background>
-      <Text>SignUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Nome completo"
+          />
+
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 };
 
 export default SignUp;
+
+SignUp.propTypes = {
+  navigation: PropTypes.object.isRequired,
+};
