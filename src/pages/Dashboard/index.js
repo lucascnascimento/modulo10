049924d0c282aaaa9fdb7fd3cb3477f7b5599438
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-07-17 14:22:40 -0300

    Cria a página de dashboard

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const Dashboard = () => {
+  return <Text>Dashboard</Text>;
+};
+
+export default Dashboard;
