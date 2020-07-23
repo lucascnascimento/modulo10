3e4de761047bf7c6403ee7917fc72d3ebc1a8a69
Lucas Cordeiro Nascimento <lucascnascimento@hotmail.com>
2020-07-23 15:09:07 -0300

    Carrega os providers da api e estiliza a página de providers

--- src/pages/New/SelectProvider/index.js
+++ src/pages/New/SelectProvider/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
-import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 
 import Background from '~/components/Background';
 
-// import { Container } from './styles';
+import api from '~/services/api';
 
-const SelectProvider = () => {
+import { Container, ProvidersList, Provider, Avatar, Name } from './styles';
+
+const SelectProvider = ({ navigation }) => {
+  const [providers, setProviders] = useState([]);
+
+  useEffect(() => {
+    async function loadProviders() {
+      const response = await api.get('providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
   return (
     <Background>
-      <Text>SelectProvider</Text>
+      <Container>
+        <ProvidersList
+          data={providers}
+          keyExtractor={(provider) => String(provider.id)}
+          renderItem={({ item: provider }) => (
+            <Provider
+              onPress={() =>
+                navigation.navigate('SelectDateTime', { provider })
+              }
+            >
+              <Avatar
+                source={{
+                  uri: provider.avatar
+                    ? provider.avatar.url
+                    : `https://api.adorable.io/avatar/50/${provider.name}.png`,
+                }}
+              />
+              <Name>{provider.name}</Name>
+            </Provider>
+          )}
+        />
+      </Container>
     </Background>
   );
 };
